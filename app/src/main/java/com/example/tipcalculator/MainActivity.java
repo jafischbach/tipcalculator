792eb010408fc9769e51e419d20de568435cb39f
Your Name <you@example.com>
2021-09-27 14:34:53 -0400

    added radio buttons

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -15,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textOutput;
     private SeekBar seekBar;
     private TextView textBarLabel;
+    private RadioGroup radioGroup;
+    private RadioButton radioParty;
+    private EditText editParty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +29,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textBarLabel = findViewById(R.id.textBarLabel);
+        initSeekBar();
+        initEditCost();
+        initRadioGroup();
+
+        textOutput = findViewById(R.id.textOutput);
+    }
+
+    private void initRadioGroup() {
+        radioGroup = findViewById(R.id.radioGroup);
+        radioParty = findViewById(R.id.radioParty);
+        editParty = findViewById(R.id.editParty);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (radioParty.isChecked()) { // Party button selected
+                    editParty.setEnabled(true);
+                } else { // Single button selected
+                    editParty.setEnabled(false);
+                }
+                calculate();
+            }
+        });
+    }
+
+    private void initEditCost() {
+        editCost = findViewById(R.id.editCost);
+        editCost.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    calculate();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initSeekBar()  {
         seekBar = findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 textBarLabel.setText(i+"");
+                calculate();
             }
 
             @Override
@@ -39,17 +85,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        editCost = findViewById(R.id.editCost);
-        textOutput = findViewById(R.id.textOutput);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                double bill = Double.parseDouble(editCost.getText().toString());
-                bill += bill*(seekBar.getProgress()/100.0);
-                textOutput.setText("$"+String.format("%.2f", bill));
-            }
-        });
     }
+
+    public void calculate() {
+        String input = editCost.getText().toString();
+        if (!input.equals("")) {
+            double bill = Double.parseDouble(input);
+            bill += bill * (seekBar.getProgress() / 100.0);
+            if (radioParty.isChecked()) {
+                double party = Double.parseDouble(editParty.getText().toString());
+                bill /= party;
+            }
+            textOutput.setText("$" + String.format("%.2f", bill));
+        }
+    }
+
 }
